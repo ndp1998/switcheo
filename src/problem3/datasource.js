0f64351f420afed5af770e4e9845686d04219cc8
ndp1998 <71389283+ndp1998@users.noreply.github.com>
2022-06-16 15:40:03 +0800

    Add files via upload

---
+++ src/problem3/datasource.js
@@ -0,0 +1,31 @@
+class Price {
+    constructor (pair, buy, sell, mid, quote) {
+        this.pair = pair;
+        this.buy = buy;
+        this.sell = sell;
+        this.mid = mid;
+        this.quote = quote;
+    }
+}
+
+class DataSource {
+    getPrices() {
+        let url = 'https://static.ngnrs.io/test/prices'
+        fetch(url)
+        .then(response=>response.json())
+        .then(data=>{
+            data.data.prices.forEach(element => {
+                let price = new Price(element.pair, element.buy, element.sell, (element.buy + element.sell)/2, element.pair.slice(3,))
+                console.log(`Mid price for ${ price.pair } is ${ price.mid } ${ price.quote }.`);
+            });
+        })
+        .catch(err => {
+            console.log("Something went wrong", err)
+        })
+    }
+}
+
+ds = new DataSource()
+ds.getPrices()
+
+    
